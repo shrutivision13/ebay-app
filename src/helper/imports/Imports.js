b46727ef1670354d26cd9b1572ca88adfd22b132
Mital vagadiya <mitalv64@gmail.com>
2024-05-23 10:45:26 +0530

    inventory api + ui

--- src/helper/imports/Imports.js
+++ src/helper/imports/Imports.js
@@ -2,7 +2,7 @@
 import { useForm, Controller } from 'react-hook-form';
 import Link from 'next/link';
 import DataTable from "react-data-table-component";
-import { IconPencil, IconTrash, IconUserPlus, IconEye, IconEyeOff, IconUser, IconSettings, IconHome2, IconNotes } from '@tabler/icons-react';
+import { IconPencil, IconTrash, IconUserPlus, IconEye, IconEyeOff, IconUser, IconSettings, IconHome2, IconNotes, IconPhoto, IconX } from '@tabler/icons-react';
 
 export {
     // react-hook-form
@@ -11,5 +11,5 @@ export {
     DataTable,
     // icons
     IconPencil, IconTrash, IconUserPlus, IconEye, IconEyeOff,
-    IconUser, IconSettings, IconHome2, IconNotes
+    IconUser, IconSettings, IconHome2, IconNotes, IconPhoto, IconX
 }
